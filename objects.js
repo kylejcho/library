8c9a68b242679878ab36561a4ae0edc042c22e2d
Kyle Cho <kylejcho@gmail.com>
2021-11-22 21:38:11 -0500

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,10 @@
+function books() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    function info() {
+        return title + ', ' + pages + ' pages ,' + read;
+    }
+}
+
