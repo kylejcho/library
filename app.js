904a8209ffaef75d280f869d834e2f0fecf2d4b7
Kyle Cho <kylejcho@gmail.com>
2021-11-23 20:33:46 -0500

    Book properties

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 let myLibrary = []
 
-function book() {
+const aBook = document.querySelector(".book");
+
+const book1 = new Book("Animal Farm", "George Orwell", 215, true);
+
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    function info() {
-        return title + ', ' + pages + ' pages ,' + read;
-    }
+    this.isRead = isRead;
+    this.info = () => {
+        return "Title: " + this.title + ", Author: " + this.author + ", Pages: " + this.pages + ", read: " + this.isRead;
+    } 
 }
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
 }
+
+addBookToLibrary(book1);
+
+
+
+const showMyLibrary = myLibrary.map(bookProp => bookProp.info());
+
+console.log(showMyLibrary)
+
+aBook.append(showMyLibrary);
